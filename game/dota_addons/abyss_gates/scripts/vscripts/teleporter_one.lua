186205eab22459f0b2aabf0e46c5a35fca1deff3
cowpeep <cowpeep@gmail.com>
2015-06-25 20:56:00 -0400

    Finished Teleporters

--- game/dota_addons/abyss_gates/scripts/vscripts/teleporter_one.lua
+++ game/dota_addons/abyss_gates/scripts/vscripts/teleporter_one.lua
@@ -1,5 +1,5 @@
 function OnStartTouch(trigger)
-    local point = Entities:FindByName( nil, "point_teleport"):GetAbsOrigin()
+    local point = Entities:FindByName( nil, "point_teleport_shopright"):GetAbsOrigin()
     FindClearSpaceForUnit(trigger.activator, point, false)
     trigger.activator:Stop()
     SendToConsole("dota_camera_center")
